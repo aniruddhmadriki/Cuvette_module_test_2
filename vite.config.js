73882ef140b0d7eb607253e2986090e4e18efde6
aniruddhmadriki <64469268+aniruddhmadriki@users.noreply.github.com>
2023-12-15 22:29:02 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
+  alias: {
+    "@": path.resolve(__dirname, "./src"),
+  }
+}
 })
